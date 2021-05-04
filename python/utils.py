ef678b082ea4d43d56018967e8ecd46f6400a4f5
kvgarimella <kvgarimella@gmail.com>
2021-05-04 16:12:51 -0400

    fix device issues

--- python/utils.py
+++ python/utils.py
@@ -5,19 +5,19 @@ Inspiration from: # https://stackoverflow.com/questions/12681945/reversing-bits-
 import torch
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print("Device for loading bit reversal tables..", device)
-LUT0  = torch.load("bit_reversal_tables/LUT0.pt").to(device)
-LUT1  = torch.load("bit_reversal_tables/LUT1.pt").to(device)
-LUT2  = torch.load("bit_reversal_tables/LUT2.pt").to(device)
-LUT3  = torch.load("bit_reversal_tables/LUT3.pt").to(device)
-LUT4  = torch.load("bit_reversal_tables/LUT4.pt").to(device)
-LUT5  = torch.load("bit_reversal_tables/LUT5.pt").to(device)
-LUT6  = torch.load("bit_reversal_tables/LUT6.pt").to(device)
-LUT7  = torch.load("bit_reversal_tables/LUT7.pt").to(device)
-LUT8  = torch.load("bit_reversal_tables/LUT8.pt").to(device)
-LUT9  = torch.load("bit_reversal_tables/LUT9.pt").to(device)
-LUT10 = torch.load("bit_reversal_tables/LUT10.pt").to(device)
-LUT11 = torch.load("bit_reversal_tables/LUT11.pt").to(device)
-LUT12 = torch.load("bit_reversal_tables/LUT12.pt").to(device)
+LUT0  = torch.load("bit_reversal_tables/LUT0.pt").to(device);
+LUT1  = torch.load("bit_reversal_tables/LUT1.pt").to(device);
+LUT2  = torch.load("bit_reversal_tables/LUT2.pt").to(device);
+LUT3  = torch.load("bit_reversal_tables/LUT3.pt").to(device);
+LUT4  = torch.load("bit_reversal_tables/LUT4.pt").to(device);
+LUT5  = torch.load("bit_reversal_tables/LUT5.pt").to(device);
+LUT6  = torch.load("bit_reversal_tables/LUT6.pt").to(device);
+LUT7  = torch.load("bit_reversal_tables/LUT7.pt").to(device);
+LUT8  = torch.load("bit_reversal_tables/LUT8.pt").to(device);
+LUT9  = torch.load("bit_reversal_tables/LUT9.pt").to(device);
+LUT10 = torch.load("bit_reversal_tables/LUT10.pt").to(device);
+LUT11 = torch.load("bit_reversal_tables/LUT11.pt").to(device);
+LUT12 = torch.load("bit_reversal_tables/LUT12.pt").to(device);
 
 def get_indices(array_size):
     if array_size == 1:
@@ -68,7 +68,8 @@ def mod_exp(b,e,m):
     b = torch.remainder(b,m)
     gtz = e > 0
     while torch.any(gtz):
-        r[gtz] = torch.where((e[gtz]&1).type(torch.bool),torch.remainder(r[gtz]*b[gtz],m),r[gtz])
+        condition = (e[gtz] & 1).type(torch.bool).to(device)
+        r[gtz] = torch.where(condition,torch.remainder(r[gtz]*b[gtz],m),r[gtz])
         e[gtz]>>=1
         b[gtz] = torch.remainder(b[gtz]**2, m)
         gtz = e > 0
