2092a30e921e5e6b6a23ca9ce627df787950fabc
kvgarimella <kvgarimella@gmail.com>
2021-05-12 15:56:38 -0400

    add support at high level for matrix

--- pytorch_gpu/main.py
+++ pytorch_gpu/main.py
@@ -1,20 +1,26 @@
 import sys
 import ntt
+import math
 import torch
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print("Performing NTT on.......................", device)
 
-if len(sys.argv) < 2:
-    print("You must enter a size for the vector! Please make it a power of 2")
+if len(sys.argv) < 3:
+    print("You must enter a size for the vector and the number of vectors!")
+    print("Please make the size of the vector to be a power of 2!")
     exit()
 
 n = int(sys.argv[1])
+d = int(sys.argv[2])
+
 p = 1073750017
 r = 5 
+print(n,d)
 
-
-vec     = torch.arange(n, device=device)
-outvec  = ntt.in_place_ntt(vec, p, r)
-print(outvec)
+vec = torch.arange(n, device=device)
+vec = vec.repeat(d).reshape(d,n)
+print(vec.shape)
+#outvec  = ntt.in_place_ntt(vec, p, r)
+#print(outvec)
